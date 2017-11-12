4a437ab3b1b08c890b52c90906088052873ae0a2
Tyler Pepper <tyler@intergalactic.com>
2017-11-12 00:34:37 -0800

    Remove additional font weights.

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends PureComponent {
         <Helmet>
           <title>Sample Code | { defaultName }</title>
           <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' />
-          <link href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:300,400,500" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:300" rel="stylesheet" />
         </Helmet>
         <Header />
         <Tray />
